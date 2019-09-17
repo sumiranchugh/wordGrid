06d9cd1e2166ed7e9aa60db824f76a36fc582e52
sumiranchugh <19881211cs>
2019-09-17 18:22:56 +1000

    case insensitive fix

--- Dictionary.py
+++ Dictionary.py
@@ -4,14 +4,14 @@ class Dictionary:
     def __init__(self, path):
         self.words = []
         with open(path, "r") as f:
-            self.words = f.read().split('\n')
+            self.words = f.read().lower().split('\n')
 
     def is_valid_word(self, word, exclude_list=[]):
-        if word in self.words and word not in exclude_list:
+        if word.lower() in self.words and word not in exclude_list:
             return True
 
     def is_partial_match(self, word):
         for w in self.words:
-            if w.startswith(word):
+            if w.startswith(word.lower()):
                 return True
         return False
